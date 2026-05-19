8c428e6f29e9f0474109af5d26f291475cf43313
lobarramatova <lobarrramatova21@gmail.com>
2026-05-19 17:27:54 +0500

    third commit

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Assalomu alekum, botimizga xush kelibsiz!")
+	bot.reply_to(message, " Assalomu alaykum! Botimizga xush kelibsiz")
 	
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-
+	print(message)
 	text = message.text
 	if text.isascii():
 		bot.reply_to(message, to_cyrillic(text))
